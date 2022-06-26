c7bcec3035e14a3642ab181a741686990337f4c3
Ezzywealth <geniuswealthb@gmail.com>
2022-06-26 18:44:43 +0100

    undo background image changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,21 +7,15 @@ module.exports = {
         Roboto: ["Roboto"],
       },
       backgroundImage: {
-        image1: " url('/src/assets/image1.png')",
-        image3: " url('/src/assets/image3.png')",
-        image4: "url('/src/assets/hotels/Rectangle 30.png')",
-        image5: " url('/src/assets/facilites/Rectangle 50.png')",
+        image1:
+          "linear-gradient(to bottom right, rgba(21, 45, 30,0.4), rgba(21, 45, 30,0.4)), url('/src/assets/image1.png')",
+        image3:
+          "linear-gradient(to bottom right, rgba(21, 45, 30,0.7), rgba(21, 45, 30,0.7)), url('/src/assets/image3.png')",
+        image4:
+          "linear-gradient(to bottom right, rgba(114, 44, 17, 0.4), rgba(114, 44, 17, 0.4)), url('/src/assets/hotels/Rectangle 30.png')",
+        image5:
+          "linear-gradient(to bottom right, rgba(21, 45, 30, 0.5), rgba(21, 45, 30, 0.5)), url('/src/assets/facilites/Rectangle 50.png')",
       },
-      // backgroundImage: {
-      //   image1:
-      //     "linear-gradient(to bottom right, rgba(21, 45, 30,0.4), rgba(21, 45, 30,0.4)), url('/src/assets/image1.png')",
-      //   image3:
-      //     "linear-gradient(to bottom right, rgba(21, 45, 30,0.7), rgba(21, 45, 30,0.7)), url('/src/assets/image3.png')",
-      //   image4:
-      //     "linear-gradient(to bottom right, rgba(114, 44, 17, 0.4), rgba(114, 44, 17, 0.4)), url('/src/assets/hotels/Rectangle 30.png')",
-      //   image5:
-      //     "linear-gradient(to bottom right, rgba(21, 45, 30, 0.5), rgba(21, 45, 30, 0.5)), url('/src/assets/facilites/Rectangle 50.png')",
-      // },
     },
   },
   plugins: [],
